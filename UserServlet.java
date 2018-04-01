2a2d362201b23a4adc0fe0bcad8d43b6232e02f0
linkai1994 <36360763+linkai1994@users.noreply.github.com>
2018-04-01 15:58:55 +0800

    Create UserServlet.java

---
+++ UserServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserServlet extends ParentNewServlet{
+	public void init(){
+		this.className="Dao_User";
+		
+	}
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+		super.doGet(request, response);
+	}
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+		doGet(request, response);
+	}
+}
